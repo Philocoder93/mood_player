d898d1759151ea842447314152d472dac9591349
ISAAC BENJAMIN DUARTE <Isaac.b.duarte@icloud.com>
2017-03-15 13:47:05 -0400

    only angular

--- db/migrate/20170312164503_create_songs.rb
+++
@@ -1,9 +0,0 @@
-class CreateSongs < ActiveRecord::Migration[5.0]
-  def change
-    create_table :songs do |t|
-      t.string :uri
-      t.references :mood
-      t.timestamps
-    end
-  end
-end
